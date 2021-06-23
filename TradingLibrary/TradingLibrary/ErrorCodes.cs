eaf54d44e1e731ea8778fd6277ada6fb41210f44
Lewis C. Parr <lewisparr@gmail.com>
2021-06-23 16:23:37 +0100

    Create ErrorCodes.cs

---
+++ TradingLibrary/TradingLibrary/ErrorCodes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingLibrary
+{
+    public static class ErrorCodes
+    {
+        // Transaction
+        public static string NEGATIVE_AMOUNT = "Amount is negative";
+        public static string ZERO_AMOUNT = "Amount is zero";
+
+        // Binance
+        public static string UNKNOWN_TYPE = "Unknown transaction type '{0}'";
+        public static string BLANK_MARKET = "Market is blank";
+        public static string UNKNOWN_MARKET = "Unknown market '{0}'";
+    }
+}
